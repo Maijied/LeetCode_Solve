25593758ca1987f4aba5c7f940a9a5f70de4d9ad
unknown <mdshuvo40@gmail.com>
2022-05-26 17:22:13 +0600

    LeetCode problem Solution

---
+++ 9_Palindrome Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+    var reverse = "";
+    var reverseX = x.toString();
+    for(var i = reverseX.length - 1; i >= 0; i--){
+        reverse += reverseX[i];
+    }
+    var reverseToNum = parseInt(reverse);
+    if(reverseToNum === x) {
+        return true;
+    }
+    else if(reverseToNum !== x){
+     return false;   
+    }
+};

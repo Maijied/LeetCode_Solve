c0df409d7b28ee3bc039f54ae963aae2d093d115
Maizied Hasan <mdshuvo40@gmail.com>
2022-12-31 18:46:31 +0600

    Updated solution : 136

--- 136_Single Number.js
+++ 136_Single Number.js
@@ -3,28 +3,31 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let array = nums;
-    let l = array.length,i, j,unique = [];
+    //Method 1
+    // let array = nums;
+    // let l = array.length,i, j,unique = [];
+    // for (i = 0; i < l; i++) {
+    //     for (j = 0; j < l; j++) {
+    //         if (i === j) {
+    //             continue;
+    //         }
+    //         if (array[i] === array[j]) {
+    //             break;
+    //         }
+    //     }
+    //     if (j === l) {
+    //         unique.push(array[i]);
+    //     }
+    // }
+    // return unique; //Runtime ->357 ms
 
-    for (i = 0; i < l; i++) {
-        for (j = 0; j < l; j++) {
-            if (i === j) {
-                continue;
-            }
-            if (array[i] === array[j]) {
-                break;
-            }
-        }
-        if (j === l) {
-            unique.push(array[i]);
-        }
-    }
-    return unique; //Runtime ->357 ms
+    // Method 2
+    // return nums.filter((v, i) => nums.indexOf(v) === nums.lastIndexOf(v)); //1000 ms
+
+    // Method 3
+    return nums.reduce((acc, cur) => {
+        console.log(acc,cur);
+            return acc ^ cur;
+        }, 0) //Runtime327 ms
+        
 };
-
-
-
-
-// Method 2
-let  unique = nums.filter((v, i) => nums.indexOf(v) === nums.lastIndexOf(v));
-   return unique;

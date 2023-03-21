682e45a31be9fa74be7be28650fc6ce59028192c
maijied <mdshuvo40@gmail.com>
2023-03-21 16:23:35 +0600

    605->updated approach 1

--- 605_Can Place Flowers.js
+++ 605_Can Place Flowers.js
@@ -5,21 +5,14 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
     // Approach 1
-        // Why this approch is not working?
-        var flowerbadCount = flowerbed.length;
-        // var count = 0;
-        // for(let i = 0; i < flowerbadCount; ++i){
-        //     if(flowerbed[i] == flowerbed[i+1]){
-        //         count++;
-        //         console.log('count=', count, 'n=',n)
-        //         if(count == n){
-        //             return true
-        //         } 
-        //         else{
-        //             return false;
-        //         } 
-        //     }
-        // }
+    var flowerbadCount = flowerbed.length;
+    for(let i = 0; i < flowerbadCount & n !== 0; ++i){
+        if(flowerbed[i] === 0 && flowerbed[i+1] !== 1 && flowerbed[i-1] !== 1){
+            n--;
+            i++;
+        }
+    }
+    return n <= 0;
     
     // Approach 2
     
